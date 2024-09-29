6d0dcdc6a8c342f744f4ef723ab35e268cca9a7e
Syed Ali Mehdi <mehdi55shah@gmail.com>
2024-09-29 07:35:27 -0700

    allow injecting variables from process.env

--- src/Base.ts
+++ src/Base.ts
@@ -25,39 +25,44 @@ const SalSettings = (config?: ISalSettingsConfig) => <T extends {new(...args: an
         // Assign variables to process.env & settings instance
         for(const [key, value] of list) {
             let assignableValue: any = value
-            if(state.map[key]) {
-                const validationRuleList = state.map[key];
-                if(validationRuleList.includes(ValueType.NULLABLE) && !value) {}
-                else for(const type of validationRuleList) {
-                    if(type === ValueType.NUMBER) {
-                        assignableValue = +value
-                        if(Number.isNaN(assignableValue))
-                            throw new Error(`${key} has invalid type ${typeof value}. It must be number.`)
-                    }
-                    else if(type === ValueType.BOOLEAN) {
-                        const boolmap: Record<string, boolean> = {
-                            "true": true,
-                            "false": false,
+            if(value === "process.env") {
+                constructor.prototype[key] = process.env[key]
+            }
+            else {
+                if(state.map[key]) {
+                    const validationRuleList = state.map[key];
+                    if(validationRuleList.includes(ValueType.NULLABLE) && !value) {}
+                    else for(const type of validationRuleList) {
+                        if(type === ValueType.NUMBER) {
+                            assignableValue = +value
+                            if(Number.isNaN(assignableValue))
+                                throw new Error(`${key} has invalid type ${typeof value}. It must be number.`)
                         }
-                        assignableValue = boolmap[value.trim()]
-                        if(assignableValue === undefined) {
-                            throw new Error(`${key} has invalid type ${typeof value}. It must be boolean.`)
+                        else if(type === ValueType.BOOLEAN) {
+                            const boolmap: Record<string, boolean> = {
+                                "true": true,
+                                "false": false,
+                            }
+                            assignableValue = boolmap[value.trim()]
+                            if(assignableValue === undefined) {
+                                throw new Error(`${key} has invalid type ${typeof value}. It must be boolean.`)
+                            }
                         }
-                    }
-                    else if(type === ValueType.STRING) {
-                        const boolmap: Record<string, boolean> = {
-                            "true": true,
-                            "false": false,
+                        else if(type === ValueType.STRING) {
+                            const boolmap: Record<string, boolean> = {
+                                "true": true,
+                                "false": false,
+                            }
+                            if(!value || !Number.isNaN(+value))
+                                throw new Error(`${key} has invalid type ${typeof value}. It must be string.`)
+                            else if (boolmap[value])
+                                throw new Error(`${key} has invalid type boolean. It must be string.`)
                         }
-                        if(!value || !Number.isNaN(+value))
-                            throw new Error(`${key} has invalid type ${typeof value}. It must be string.`)
-                        else if (boolmap[value])
-                            throw new Error(`${key} has invalid type boolean. It must be string.`)
                     }
                 }
+                process.env[key] = assignableValue;
+                constructor.prototype[key] = assignableValue;
             }
-            process.env[key] = assignableValue;
-            constructor.prototype[key] = assignableValue;
         }
     } catch (error: any) {
         if(error?.message?.includes("has invalid type")) {
